f54865d61cb76ff81b4b8a4e4641c8f9e9600a51
Vinay Pratap Singh <jadonvinay8@gmail.com>
2020-10-20 16:30:14 +0530

    Changed Configurations for dynamoDB bean

--- src/main/java/com/capgemini/LocationService/configuration/DynamoDBConfig.java
+++ src/main/java/com/capgemini/LocationService/configuration/DynamoDBConfig.java
@@ -2,8 +2,10 @@ package com.capgemini.LocationService.configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,27 +16,33 @@ import org.springframework.util.StringUtils;
 @EnableDynamoDBRepositories(basePackages = "com.capgemini.LocationService")
 public class DynamoDBConfig {
 
-    @Value("${amazon.dynamodb.endpoint}")
-    private String amazonDynamoDBEndpoint;
+//    @Value("${amazon.dynamodb.endpoint}")
+//    private String amazonDynamoDBEndpoint;
+//
+//    @Value("${amazon.aws.accesskey}")
+//    private String amazonAWSAccessKey;
+//
+//    @Value("${amazon.aws.secretkey}")
+//    private String amazonAWSSecretKey;
 
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
+//    @Bean
+//    public AmazonDynamoDB amazonDynamoDB() {
+//        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
+//
+//        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
+//            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+//        }
+//        return amazonDynamoDB;
+//    }
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(amazonAWSCredentials());
-
-        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
-            amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
-        }
-        return amazonDynamoDB;
+        return AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1).build();
     }
 
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-    }
+//    @Bean
+//    public AWSCredentials amazonAWSCredentials() {
+//        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+//    }
 }
