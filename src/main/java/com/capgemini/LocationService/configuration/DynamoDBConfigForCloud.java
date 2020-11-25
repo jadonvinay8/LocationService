d650a2e24d6aa41042eac7d936128f94ea1834c3
Vinay Pratap Singh <jadonvinay8@gmail.com>
2020-11-25 13:38:12 +0530

    Added cloud config for DynamoDB

---
+++ src/main/java/com/capgemini/LocationService/configuration/DynamoDBConfigForCloud.java
@@ -0,0 +1,38 @@
+package com.capgemini.LocationService.configuration;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.capgemini.LocationService.entities.City;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = "com.capgemini.LocationService")
+public class DynamoDBConfigForCloud {
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard()
+          .withRegion(Regions.US_EAST_1)
+          .build();
+    }
+
+    @PostConstruct
+    private void setupTables() {
+        AmazonDynamoDB amazonDynamoDB = amazonDynamoDB();
+        DynamoDBMapper mapper = new DynamoDBMapper(amazonDynamoDB);
+
+        CreateTableRequest tableRequest = mapper.generateCreateTableRequest(City.class);
+        tableRequest.setProvisionedThroughput(new ProvisionedThroughput(5L, 5L));
+        TableUtils.createTableIfNotExists(amazonDynamoDB, tableRequest);
+    }
+
+}
